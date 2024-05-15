929651b6beb8050f62e3a5c9ad899ff0945f6d26
aiPenguin <dzfwang@outlook.com>
2024-05-15 23:57:58 +0200

    fix mappings of gitsigns

--- lua/configs/overrides.lua
+++ lua/configs/overrides.lua
@@ -81,7 +81,52 @@ M.telescope = {
 }
 
 M.gitsigns = {
-  
+  on_attach = function(bufnr)
+    local gitsigns = package.loaded.gitsigns
+    local function opts(desc)
+      return { buffer = bufnr, desc = desc }
+    end
+
+    local map = vim.keymap.set
+
+
+    -- Navigation
+    map('n', ']c', function()
+      if vim.wo.diff then
+        vim.cmd.normal({']c', bang = true})
+      else
+        gitsigns.nav_hunk('next')
+      end
+    end,
+    opts "Gitsigns Next Hunk")
+
+    map('n', '[c', function()
+      if vim.wo.diff then
+        vim.cmd.normal({'[c', bang = true})
+      else
+        gitsigns.nav_hunk('prev')
+      end
+    end,
+    opts "Gitsigns Prev Hunk")
+
+    -- Actions
+    map('n', '<leader>hs', gitsigns.stage_hunk, opts "Gitsigns Stage Hunk")
+    map('n', '<leader>hr', gitsigns.reset_hunk, opts "Gitsigns Reset Hunk")
+    map('v', '<leader>hs', function() gitsigns.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end, opts "Gitsigns Stage Hunk")
+    map('v', '<leader>hr', function() gitsigns.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end, opts "Gitsigns Reset Hunk")
+    map('n', '<leader>hS', gitsigns.stage_buffer, opts "Gitsigns Stage Buffer")
+    map('n', '<leader>hu', gitsigns.undo_stage_hunk, opts "Gitsigns Undo Stage Hunk")
+    map('n', '<leader>hR', gitsigns.reset_buffer, opts "Gitsigns Reset Buffer")
+    map('n', '<leader>hp', gitsigns.preview_hunk, opts "Gitsigns Preview Hunk")
+    map('n', '<leader>hb', function() gitsigns.blame_line{full=true} end, opts "Gitsigns Blame Line")
+    map('n', '<leader>tb', gitsigns.toggle_current_line_blame, opts "Gitsigns Toggle Blame Line")
+    map('n', '<leader>hd', gitsigns.diffthis, opts "Gitsigns Diff This")
+    map('n', '<leader>hD', function() gitsigns.diffthis('~') end, opts "Gitsigns Diff This (cached)")
+    map('n', '<leader>td', gitsigns.toggle_deleted, opts "Gitsigns Toggle Deleted")
+
+    -- Text object
+    map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>', opts "Gitsigns Select Hunk")
+  end
 }
 
 return M
